4984f4c914f7a978c071b64e7e7f204b91f7f8da
Vladislav-Peryazev77 <vladperyazev14@gmail.com>
2024-02-21 17:45:32 +0700

    added slider to coaches block

--- src/components/pagesComponents/homePage/CoachesBlock/CoachesBlock.jsx
+++ src/components/pagesComponents/homePage/CoachesBlock/CoachesBlock.jsx
@@ -3,6 +3,7 @@ import styles from "./CoachesBlock.module.scss";
 
 import coachesData from "../../../../api/coaches.json";
 import { coachesImages } from "../../../../assets/images";
+import { Button } from "../../../UIComponents/Button";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
@@ -13,29 +14,43 @@ import "swiper/css/scrollbar";
 
 export const CoachesBlock = () => {
   return (
-    <div className="container">
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        loop
-        freeMode
-        // pagination={{ clickable: true }}
-        // scrollbar={{ draggable: true }}
-      >
-        {coachesData.map((coach) => {
-          return (
-            <SwiperSlide>
-              <div style={{ width: "360px", height: "660px" }}>
-                <img src={coachesImages[coach.imageName]} alt="" />
-                <div>{coach.title}</div>
-                <div>{coach.jobtitle}</div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </div>
+    <section className={styles.coaches}>
+      <div className="container">
+        <h2>Профессиональные тренеры</h2>
+        <Swiper
+          className={styles.slider}
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={40}
+          slidesPerView={3}
+          navigation
+          // loop
+          freeMode
+          // pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+        >
+          {coachesData.map((coach) => {
+            return (
+              <SwiperSlide>
+                <div className={styles.item}>
+                  <img
+                    className={styles.photo}
+                    src={coachesImages[coach.imageName]}
+                    alt={coach.title}
+                  />
+                  <div className={styles.title}>{coach.title}</div>
+                  <div className={styles.jobtitle}>{coach.jobtitle}</div>
+                  <Button
+                    variant={styles.button__red}
+                    additionalClassname={styles.button}
+                  >
+                    Подробнее
+                  </Button>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { BenefitsBlock } from "../../components/pagesComponents/homePage/BenefitsBlock";
+import { CoachesBlock } from "../../components/pagesComponents/homePage/CoachesBlock/CoachesBlock";
 import { PackagesBlock } from "../../components/pagesComponents/homePage/PackagesBlock";
 
 import { PromoBlock } from "../../components/pagesComponents/homePage/PromoBlock";
@@ -12,6 +13,7 @@ export const Home = () => {
       <PromoBlock />
       <StatsBlock />
       <BenefitsBlock />
+      <CoachesBlock />
       <PackagesBlock />
       <RegistrationBlock />
     </>
